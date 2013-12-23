7c4488a9311e46436fecf55c4cb17846aedea281
Zinahe <zinahe@gmail.com>
2013-12-22 23:10:45 -0500

    . Implements call-back based SysTick timers . Many instances of the Timer struct can be created with thier own intervals . Timer instances can hold cyclic internal states on each firing

--- SysTick.h
+++ SysTick.h
@@ -1,15 +1,38 @@
 #include <stdint.h>
 #include "LPC1114.h"
 
-// SysTick event counter
+typedef struct Timer {
+	uint32_t interval;
+	uint32_t counter;
+	uint32_t state;
+		
+	void (*callback)(uint32_t data);
+} Timer_t;
+
 volatile uint32_t SysTick_counter;
+volatile Timer_t *SysTick_timer;
+
+// Delarations
 void SysTick_init(void);
 void wait(uint32_t time);
+void SysTick_run(Timer_t *);
+
+
+// Definitions
+void SysTick_run(Timer_t *timer) {
+	SysTick_timer = timer;
+	
+	// Enable SysTick block
+	SYSTICK_CSR |= (1 << SYSTICK_ENABLE_BIT);
+}
 
 void wait(uint32_t time) {
 	
 	// Reset counter
 	SysTick_counter = 0;
+	
+	// Enable SysTick block
+	SYSTICK_CSR |= (1 << SYSTICK_ENABLE_BIT);
 	
 	// Wait until SysTick increments counter enough times
 	while (SysTick_counter < time);
@@ -25,9 +48,18 @@ void SysTick_init(void) {
 	SYSTICK_CVR = 0; 						
 	
 	// Enable SysTick, on CPU clock and Enable SysTick interrupt (pp. 402)
-	SYSTICK_CSR |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT);
+	//SYSTICK_CSR |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT);
+	SYSTICK_CSR |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT);
 }
 
 void SysTick_Handler(void) {
+
 	SysTick_counter++;
+	SysTick_timer->counter++;
+		
+	if (SysTick_timer->interval == SysTick_timer->counter) {
+		SysTick_timer->callback(SysTick_timer->state);
+		SysTick_timer->counter = 0;
+	}
+	
 }
