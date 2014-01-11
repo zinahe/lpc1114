01eace103c0c60b41f351311c5f6a6f5370bba54
Zinahe <zinahe@gmail.com>
2014-01-10 23:26:05 -0500

    . SysTick.c and .h updated to handle more than one timer object . Multiple timers are implemented as a linked list of timer objects . Main updated to reflect new capability
    *** Timer callbacks can NOT call wait() or any other functionality
        that relies on hardware interrupts such as I2C and LCD interfaces

--- SysTick.h
+++ SysTick.h
@@ -9,12 +9,19 @@ typedef struct Timer {
 	uint32_t counter;
 	uint32_t state;
 		
-	void (*callback)(uint32_t data);
+	//volatile struct Timer * volatile next;
+	struct Timer * next;
+	
+	//void (* volatile callback)(uint32_t data);
+	void (*callback)(uint32_t data);	
+	
 } Timer_t;
 
 // Delarations
 void SysTick_init(void);
 void wait(uint32_t time);
 void SysTick_run(Timer_t *);
+
+//void SysTick_run(volatile Timer_t * volatile);
 
 #endif
